48309d1e8a22b0bd3b1a9b5797bbb25af7fd186f
Ruochong Wu <wu1188@lpvinslogin01.itap.purdue.edu>
2018-03-21 15:38:01 -0400

    Initialized files

---
+++ lab08.c
@@ -0,0 +1,9 @@
+
+/******************************************************************************
+* Assignment:  Lab 08
+* Lab Section: Wednesday 3:30 PM SC 283
+* Description: Simulates Blackjack with two players
+* Programmers: Ruochong Wu wu1188@purdue.edu
+*              Mike Gomes-Casseres mgomesca@purdue.edu
+*              Daniel Burger burger5@purdue.edu
+******************************************************************************/
